d0690f7e0238982f07ac5f99bf6dad71325a745a
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2021-01-18 22:46:26 +0300

    5-6 less

--- dranik/news/views.py
+++ dranik/news/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    # print(request)
+    return HttpResponse('<h1>Вызвали index</hq>')
+
+
+def test(request):
+    return HttpResponse('Вызвали тестовую страницу')
