246182cbb9390301cd0a6a45fb5bc497cfbd7355
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2021-02-16 23:48:15 +0300

    2021-02-16 less 13

--- dranik/news/admin.py
+++ dranik/news/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import News, Category
 
-# Register your models here.
+
+# добавляем новые поля в таблицу для админки
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'category')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_editable = ('category',)
+    list_filter = ('category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+
+# регистрируем основную модель, изменения в админку
+# важен порядок добавления - сначало регистрируем основную модель - потом класс по ее настройке
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
--- dranik/news/views.py
+++ dranik/news/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 
 def index(request):
-    return HttpResponse('<h1>Вызвали index</hq>')
-
-
-def test(request):
-    return HttpResponse('Вызвали тестовую страницу')
+    news = News.objects.all()
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
